426988f608a6d343cf2209e65dd94b7d48f24b4a
emmanuhkipngetich <emmmanuhkipngetich@gmail.com>
2025-03-09 12:37:53 +0300

    Testrunner change

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -7,10 +7,9 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
 		plugin = {"pretty","html:target/cucumber-reports/cucumber.html","json:target/cucumber-reports/cucumber.json"},
 		monochrome = true,
-		publish = true,
 		dryRun = false)
 
-public class TestRunner extends Setup
+public class TestRunner
 {
 	
 }
