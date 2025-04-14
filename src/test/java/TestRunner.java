203d84d2332b4c8c3d7d0877fd2cd3e76976f909
emmanuhkipngetich <emmmanuhkipngetich@gmail.com>
2025-04-14 20:24:07 +0300

    test
    test

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -5,7 +5,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		plugin = {"pretty","html:target/cucumber-reports/cucumber.html","json:target/cucumber-reports/cucumber.json"})
+		//plugin = {"pretty","html:target/cucumber-reports/cucumber.html","json:target/cucumber-reports/cucumber.json"})
+	plugin = {"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"})
 public class TestRunner
 {
 	
